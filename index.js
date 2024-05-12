30059b897a8bca47373e5f241a6f11e4287b6563
Evgeniy2022 <t0lrk0ln22rus@mail.ru>
2024-05-12 16:15:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/*slider */
+
+const swiper = new Swiper(".swiper", {
+	direction: "horizontal",
+	slidesPerView: "auto",
+	spaceBetween: 16,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
+
+/*open all*/
+let moreCardsTablet = document.querySelectorAll(".cards__wrapper_hide-tablet");
+let moreCardsDesctop = document.querySelectorAll(".cards__wrapper_hide-desctop");
+
+let btnText = document.querySelector(".cards-open__more");
+
+if (window.innerWidth >= 768 && window.innerWidth < 1120) {
+  btnText.addEventListener("click", () => {
+    moreCardsTablet.forEach((item) => {
+		item.classList.toggle("cards__wrapper_hide-tablet");
+      if (item.classList.contains("cards__wrapper_hide-tablet")) {
+        btnText.innerHTML = "Показать все";
+		  btnText.classList.remove("clicked");
+      } else {
+        btnText.innerHTML = "Скрыть";
+		  btnText.classList.add("clicked");
+      }
+    });
+  });
+}
+
+
+if (window.innerWidth >= 1120) {
+  btnText.addEventListener("click", () => {
+    moreCardsDesctop.forEach((item) => {
+		item.classList.toggle("cards__wrapper_hide-desctop");
+      if (item.classList.contains("cards__wrapper_hide-desctop")) {
+			btnText.innerHTML = "Показать все";
+			btnText.classList.remove("clicked");
+      } else {
+      	btnText.innerHTML = "Скрыть";
+			btnText.classList.add("clicked");
+      }
+    });
+  });
+}
+
+
