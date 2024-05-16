626178196cee014fa23d081359ac50791dc3a40a
Evgeniy2022 <t0lrk0ln22rus@mail.ru>
2024-05-16 11:17:43 +0300

    fix last bags

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 /*slider */
 
-const swiper = new Swiper(".swiper", {
-	direction: "horizontal",
-	slidesPerView: "auto",
-	spaceBetween: 16,
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
+document.addEventListener("DOMContentLoaded", () => {
+  const width = window.innerWidth;
+  if (width < 768) {
+   	const swiper = new Swiper(".swiper", {
+      direction: "horizontal",
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
 });
 
 /*open all*/
-let moreCardsTablet = document.querySelectorAll(".cards__wrapper_hide-tablet");
-let moreCardsDesctop = document.querySelectorAll(".cards__wrapper_hide-desctop");
+let moreCardsTablet = document.querySelectorAll(".swiper-slide__wrapper--hide-tablet");
+let moreCardsDesctop = document.querySelectorAll(".swiper-slide__wrapper--hide-desctop");
 
 let btnText = document.querySelector(".cards-open");
 
 if (window.innerWidth >= 768 && window.innerWidth < 1120) {
   btnText.addEventListener("click", () => {
     moreCardsTablet.forEach((item) => {
-		item.classList.toggle("cards__wrapper_hide-tablet");
-      if (item.classList.contains("cards__wrapper_hide-tablet")) {
+		item.classList.toggle("swiper-slide__wrapper--hide-tablet");
+      if (item.classList.contains("swiper-slide__wrapper--hide-tablet")) {
         btnText.innerHTML = "Показать все";
 		  btnText.classList.remove("clicked");
       } else {
@@ -35,8 +40,8 @@ if (window.innerWidth >= 768 && window.innerWidth < 1120) {
 if (window.innerWidth >= 1120) {
   btnText.addEventListener("click", () => {
     moreCardsDesctop.forEach((item) => {
-		item.classList.toggle("cards__wrapper_hide-desctop");
-      if (item.classList.contains("cards__wrapper_hide-desctop")) {
+		item.classList.toggle("swiper-slide__wrapper--hide-desctop");
+      if (item.classList.contains("swiper-slide__wrapper--hide-desctop")) {
 			btnText.innerHTML = "Показать все";
 			btnText.classList.remove("clicked");
       } else {
